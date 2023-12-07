0025a6558447e3d86e3472b0f8aacf54b9b185bc
Max <diedietschies@gmail.com>
2023-12-07 15:47:14 +0100

    normalization + augmentation

---
+++ tools_created/augment.py
@@ -0,0 +1,24 @@
+import os
+from PIL import Image
+
+image_directory = '../../images/polyps' 
+rotation_degrees = [90, 180, 270] 
+
+# take image and rotate it about every value in degrees parameter
+def rotate_and_save(image_path, degrees):
+    with Image.open(image_path) as img:
+        for degree in degrees:
+            rotated_img = img.rotate(degree, expand=True)
+            name, ext = os.path.splitext(image_path)
+            new_image_path = f"{name}_{degree}{ext}"
+            rotated_img.save(new_image_path)
+            print(f"Saved rotated image: {new_image_path}")
+
+def process_images(directory, degrees):
+    for filename in os.listdir(directory):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')): 
+            file_path = os.path.join(directory, filename)
+            rotate_and_save(file_path, degrees)
+
+process_images(image_directory, rotation_degrees)
+
---
+++ tools_created/calc_norm.py
@@ -0,0 +1,60 @@
+import cv2
+import os
+
+def calc_norm(dirs):
+    sum_r, sum_g, sum_b = 0.0, 0.0, 0.0
+    sum_sq_diff_r, sum_sq_diff_g, sum_sq_diff_b = 0.0, 0.0, 0.0
+    i = 0
+    for dir in dirs:
+        for img_name in os.listdir(dir):
+            print(i)
+            i += 1
+            if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+                continue
+
+            img_path = os.path.join(dir, img_name)
+            image = cv2.imread(img_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+
+            sum_r += image[:, :, 0].mean()
+            sum_g += image[:, :, 1].mean()
+            sum_b += image[:, :, 2].mean()   
+
+        
+        num_images = len(os.listdir(dir))
+        mean_r = sum_r / num_images
+        mean_g = sum_g / num_images
+        mean_b = sum_b / num_images
+
+    for dir in dirs: 
+        for img_name in os.listdir(dir):
+            print(i)
+            i += 1
+            if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+                continue
+
+            img_path = os.path.join(dir, img_name)
+            image = cv2.imread(img_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+
+            sum_sq_diff_r += ((image[:, :, 0] - mean_r) ** 2).mean()
+            sum_sq_diff_g += ((image[:, :, 1] - mean_g) ** 2).mean()
+            sum_sq_diff_b += ((image[:, :, 2] - mean_b) ** 2).mean()
+
+
+        var_r = sum_sq_diff_r / num_images
+        var_g = sum_sq_diff_g / num_images
+        var_b = sum_sq_diff_b / num_images
+
+        std_r = var_r ** 0.5
+        std_g = var_g ** 0.5
+        std_b = var_b ** 0.5
+
+    print(f"mean values: r: {mean_r}, g: {mean_g}, b: {mean_b}")
+    print(f"standard deviations: r: {std_r}, g: {std_g}, b: {std_b}")
+
+
+data_dir = '../../images'
+directories =[os.path.join(data_dir, dir) for dir in os.listdir(data_dir)]
+print(directories)
+calc_norm(directories)
