d5e8b051aed41f4af97f35191d1e6f41407feb92
Max Dietsch <diedietschies@gmail.com>
2024-02-14 16:16:56 +0100

    DOS in mm_test

--- mmpretrain/models/classifiers/dos.py
+++ mmpretrain/models/classifiers/dos.py
@@ -15,6 +15,22 @@ class DOSClassifier(ImageClassifier):
         if not isinstance(self.head, DOSHead):
             raise TypeError('Head of the model should be of type DOSHead')
 
+    def forward(self,
+                inputs: torch.Tensor,
+                data_samples: Optional[List[DataSample]] = None,
+                mode: str = 'tensor'):
+
+        if mode == 'tensor':
+            feats = self.extract_feat(inputs)
+            return self.head(feats) if self.with_head else feats
+        elif mode == 'loss':
+            return self.loss(inputs, data_samples, n, w)
+        elif mode == 'predict':
+            return self.predict(inputs, data_samples)
+        else:
+            raise RuntimeError(f'Invalid mode "{mode}".')
+
+
     def loss(self, input, data_samples, n, w):
 
         deep_feats = self.extract_feat(input)
