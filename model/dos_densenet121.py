c97443ec913d399cabfbbc3adde9d6ffb51a1770
Max Dietsch <diedietschies@gmail.com>
2024-02-14 15:49:21 +0100

    DOS in mm_test

---
+++ model/dos_densenet121.py
@@ -0,0 +1,11 @@
+model = dict(
+    type = 'DOSClassifier'
+    backbone=dict(arch='121', type='DenseNet'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        in_channels=1024,
+        loss=dict(type='DOSLoss'),
+        num_classes=4,
+        topk=1,
+        type='DOSHead'),
+    )
