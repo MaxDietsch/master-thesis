e0f1c349c89ec7681e135faa97b3434677b7a497
Max <diedietschies@gmail.com>
2024-02-15 08:02:28 +0100

    DOS mm test

--- mmpretrain/models/classifiers/dos.py
+++ mmpretrain/models/classifiers/dos.py
@@ -1,7 +1,7 @@
 
 import torch
 import torch.nn as nn
-from typing import List, Optional
+from typing import List, Optional, Union, Dict
 from mmpretrain.structures import DataSample
 
 from mmpretrain.registry import MODELS
