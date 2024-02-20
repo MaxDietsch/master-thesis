d85da39b4aef3411f5096aaeb607549edba5ece3
diedietschies <diedietschies@gmail.com>
2024-02-20 10:34:47 +0100

    cosen test

--- model/densenet121.py
+++ model/densenet121.py
@@ -2,7 +2,7 @@ model = dict(
     backbone=dict(arch='121', type='DenseNet'),
     head=dict(
         in_channels=1024,
-        loss=dict(loss_weight=1.0, type='CrossEntropyLoss', use_soft = True),
+        loss=dict(loss_weight=1.0, type='CrossEntropyLoss', use_soft = False),
         num_classes=4,
         topk=1,
         type='LinearClsHead'),
