70454d21edcb4d34fb7f799fdf5003fa53a529b6
diedietschies <diedietschies@gmail.com>
2024-02-20 08:44:54 +0100

    cosen test

--- mmpretrain/models/losses/cosen_ce_loss.py
+++ mmpretrain/models/losses/cosen_ce_loss.py
@@ -55,17 +55,15 @@ class CoSenCrossEntropyLoss(nn.Module):
                 v_b (torch.Tensor): The vectorized xi tensor, like in the paper.
         """
         v_b = self.xi.view(-1, 1)
-        print(v_a.shape)
-        print(v_b.shape)
         return -(v_a - v_b) * torch.ones((self.num_classes * self.num_classes, 1))
 
-    def update_xi(self, v_a, v_b):
+    def update_xi(self, v_a):
         """
         Args:   v_a (torch.Tensor): The vectorized T tensor, like in the paper.
                 v_b (torch.Tensor): The vectorized xi tensor, like in the paper.
         """
 
-        self.xi -= self.learning_rate * self.compute_grad(v_a, v_b)
+        self.xi -= self.learning_rate * self.compute_grad(v_a)
 
     def set_xi_lr(self, new_lr):
         """
