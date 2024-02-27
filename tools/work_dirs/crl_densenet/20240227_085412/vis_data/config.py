60da1c8510c129cf2b14724a1c3fe7e70b2d4997
Max Dietsch <diedietschies@gmail.com>
2024-02-27 14:38:54 +0000

    crl test

---
+++ tools/work_dirs/crl_densenet/20240227_085412/vis_data/config.py
@@ -0,0 +1,190 @@
+data_preprocessor = dict(
+    mean=[
+        151.78,
+        103.29,
+        97.41,
+    ], std=[
+        69.92,
+        55.96,
+        54.84,
+    ], to_rgb=True)
+dataset_type = 'CustomDataset'
+default_hooks = dict(
+    checkpoint=dict(interval=5, max_keep_ckpts=5, type='CheckpointHook'),
+    logger=dict(interval=2014, type='LoggerHook'),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    timer=dict(type='IterTimerHook'),
+    visualization=dict(enable=False, type='VisualizationHook'))
+default_scope = 'mmpretrain'
+env_cfg = dict(
+    cudnn_benchmark=False,
+    dist_cfg=dict(backend='nccl'),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0))
+k = 3
+launcher = 'none'
+load_from = None
+log_level = 'INFO'
+max_thrs = 0.2
+min_classes = [
+    1,
+    2,
+    3,
+]
+min_thrs = 0.3
+model = dict(
+    backbone=dict(arch='121', type='DenseNet'),
+    head=dict(
+        in_channels=1024,
+        loss=dict(loss_weight=1.0, type='CrossEntropyLoss', use_soft=False),
+        num_classes=4,
+        topk=1,
+        type='LinearClsHead'),
+    neck=dict(type='GlobalAveragePooling'),
+    type='ImageClassifier')
+optim_wrapper = dict(
+    optimizer=dict(lr=0.01, momentum=0.9, type='SGD', weight_decay=0.0001))
+param_scheduler = dict(
+    by_epoch=True, gamma=0.1, milestones=[
+        100,
+        200,
+        300,
+    ], type='MultiStepLR')
+resume = False
+test_cfg = dict()
+test_dataloader = dict(
+    batch_size=8,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        ann_file='meta/val.txt',
+        classes=[
+            'normal',
+            'polyps',
+            'esophagitis',
+            'barretts',
+        ],
+        data_prefix='val',
+        data_root='../../B_E_P_N-without-mix',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(interpolation='bicubic', scale=(
+                640,
+                640,
+            ), type='Resize'),
+            dict(type='PackInputs'),
+        ],
+        type='CustomDataset',
+        with_label=True),
+    num_workers=5,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+test_evaluator = [
+    dict(topk=1, type='Accuracy'),
+    dict(
+        average=None,
+        items=[
+            'precision',
+            'recall',
+            'f1-score',
+        ],
+        type='SingleLabelMetric'),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(interpolation='bicubic', scale=(
+        640,
+        640,
+    ), type='Resize'),
+    dict(type='PackInputs'),
+]
+train_cfg = dict(
+    by_epoch=4,
+    k=3,
+    max_epochs=400,
+    max_thrs=0.2,
+    min_classes=[
+        1,
+        2,
+        3,
+    ],
+    min_thrs=0.3,
+    val_interval=1)
+train_dataloader = dict(
+    batch_size=8,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        ann_file='meta/train.txt',
+        classes=[
+            'normal',
+            'polyps',
+            'esophagitis',
+            'barretts',
+        ],
+        data_prefix='train',
+        data_root='../../B_E_P_N-without-mix',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(interpolation='bicubic', scale=(
+                640,
+                640,
+            ), type='Resize'),
+            dict(type='PackInputs'),
+        ],
+        type='CustomDataset',
+        with_label=True),
+    num_workers=5,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=True, type='DefaultSampler'))
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(interpolation='bicubic', scale=(
+        640,
+        640,
+    ), type='Resize'),
+    dict(type='PackInputs'),
+]
+val_cfg = dict()
+val_dataloader = dict(
+    batch_size=8,
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        ann_file='meta/val.txt',
+        classes=[
+            'normal',
+            'polyps',
+            'esophagitis',
+            'barretts',
+        ],
+        data_prefix='val',
+        data_root='../../B_E_P_N-without-mix',
+        pipeline=[
+            dict(type='LoadImageFromFile'),
+            dict(interpolation='bicubic', scale=(
+                640,
+                640,
+            ), type='Resize'),
+            dict(type='PackInputs'),
+        ],
+        type='CustomDataset',
+        with_label=True),
+    num_workers=5,
+    persistent_workers=True,
+    pin_memory=True,
+    sampler=dict(shuffle=False, type='DefaultSampler'))
+val_evaluator = [
+    dict(topk=1, type='Accuracy'),
+    dict(
+        average=None,
+        items=[
+            'precision',
+            'recall',
+            'f1-score',
+        ],
+        type='SingleLabelMetric'),
+]
+vis_backends = [
+    dict(type='LocalVisBackend'),
+]
+work_dir = './work_dirs/crl_densenet'
--- tools_created/calc_norm.py
+++ tools_created/calc_norm.py
@@ -1,61 +1,64 @@
 import cv2
 import os
 
-def calc_norm(dirs):
+def calc_norm(dir):
     sum_r, sum_g, sum_b = 0.0, 0.0, 0.0
     sum_sq_diff_r, sum_sq_diff_g, sum_sq_diff_b = 0.0, 0.0, 0.0
     i = 0
     num_images = 0
-    for dir in dirs:
-        for img_name in os.listdir(dir):
-            print(i)
-            i += 1
-            num_images += 1
-            if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-                continue
+    #when normalize with data of val and test
+    #for dir in dirs:
+    #   for img_name in os.listdir(dir)
+    for img_name in os.listdir(dir):
+        print(i)
+        i += 1
+        num_images += 1
+        if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            continue
 
-            img_path = os.path.join(dir, img_name)
-            image = cv2.imread(img_path)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+        img_path = os.path.join(dir, img_name)
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
 
-            sum_r += image[:, :, 0].mean()
-            sum_g += image[:, :, 1].mean()
-            sum_b += image[:, :, 2].mean()   
+        sum_r += image[:, :, 0].mean()
+        sum_g += image[:, :, 1].mean()
+        sum_b += image[:, :, 2].mean()   
 
         
-        mean_r = sum_r / num_images
-        mean_g = sum_g / num_images
-        mean_b = sum_b / num_images
+    mean_r = sum_r / num_images
+    mean_g = sum_g / num_images
+    mean_b = sum_b / num_images
 
-    for dir in dirs: 
-        for img_name in os.listdir(dir):
-            print(i)
-            i += 1
-            if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-                continue
+    #for dir in dirs: 
+    #    for img_name in os.listdir(dir):
+    for img_name in os.listdir(dir):
+        print(i)
+        i += 1
+        if not img_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            continue
 
-            img_path = os.path.join(dir, img_name)
-            image = cv2.imread(img_path)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
+        img_path = os.path.join(dir, img_name)
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
 
-            sum_sq_diff_r += ((image[:, :, 0] - mean_r) ** 2).mean()
-            sum_sq_diff_g += ((image[:, :, 1] - mean_g) ** 2).mean()
-            sum_sq_diff_b += ((image[:, :, 2] - mean_b) ** 2).mean()
+        sum_sq_diff_r += ((image[:, :, 0] - mean_r) ** 2).mean()
+        sum_sq_diff_g += ((image[:, :, 1] - mean_g) ** 2).mean()
+        sum_sq_diff_b += ((image[:, :, 2] - mean_b) ** 2).mean()
 
 
-        var_r = sum_sq_diff_r / num_images
-        var_g = sum_sq_diff_g / num_images
-        var_b = sum_sq_diff_b / num_images
+    var_r = sum_sq_diff_r / num_images
+    var_g = sum_sq_diff_g / num_images
+    var_b = sum_sq_diff_b / num_images
 
-        std_r = var_r ** 0.5
-        std_g = var_g ** 0.5
-        std_b = var_b ** 0.5
+    std_r = var_r ** 0.5
+    std_g = var_g ** 0.5
+    std_b = var_b ** 0.5
 
     print(f"mean values: r: {mean_r}, g: {mean_g}, b: {mean_b}")
     print(f"standard deviations: r: {std_r}, g: {std_g}, b: {std_b}")
 
 
-data_dir = '../../rBP_B_E_P_N-without-mix'
-directories =[os.path.join(data_dir, dir) for dir in os.listdir(data_dir)]
-print(directories)
-calc_norm(directories)
+data_dir = '../../B_E_P_N/train'
+#directories =[os.path.join(data_dir, dir) for dir in os.listdir(data_dir)]
+#print(directories)
+calc_norm(data_dir)
