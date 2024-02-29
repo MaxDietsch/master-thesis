9dd00f5f970f3bb9be625a26df79f7261ad2c0b0
diedietschies <diedietschies@gmail.com>
2024-02-29 12:32:56 +0100

    crl test

--- schedule/sgd_decr.py
+++ schedule/sgd_decr.py
@@ -2,7 +2,7 @@ optim_wrapper = dict(
     optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
 
 param_scheduler = dict(
-    type='MultiStepLR', by_epoch=True, milestones=[30, 60, 100], gamma=0.1)
+    type='MultiStepLR', by_epoch=True, milestones=[30, 60, 100], gamma=0.5)
 
 train_cfg = dict(by_epoch=True, max_epochs=100, val_interval=1)
 
