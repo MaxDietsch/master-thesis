2fe53ef367f04a80a27f800edfac8d584d5e949a
diedietschies <diedietschies@gmail.com>
2024-02-29 14:03:27 +0100

    crl test

---
+++ config/phase_1/efficientnet_b7_sgd0_01.py
@@ -0,0 +1,9 @@
+_base_ = [
+        '../../model/efficientnet_b7.py',
+        '../../data/phase1/bepn16.py',
+        '../../schedule/sgd0_01.py',
+        '../../runtime/default.py'
+        ]
+
+load_from = None
+resume = False
