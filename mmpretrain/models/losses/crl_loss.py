a1ea258b9b45948036dd11b47c6452c5252e7f2a
diedietschies <diedietschies@gmail.com>
2024-02-29 08:22:08 +0100

    crl test

--- mmpretrain/models/losses/crl_loss.py
+++ mmpretrain/models/losses/crl_loss.py
@@ -259,9 +259,9 @@ class CRLLoss(nn.Module):
             for k, hard_pos in enumerate(hard_samples[1][lab]):
                 for j, hard_neg in enumerate(hard_samples[0][lab]):
 
-                    triplets[i * num_sam + j * self.k + k, 0 : num_classes] = min_sample
-                    triplets[i * num_sam + j * self.k + k, num_classes : num_classes + num_classes] = cls_score[hard_neg[1]] if hard_neg[1] > 0 else hard_neg[0]
-                    triplets[i * num_sam + j * self.k + k, 2 * num_classes : 3 * num_classes] = cls_score[hard_pos[1]] if hard_pos[1] > 0 else hard_pos[0]
+                    triplets[i * num_sam + k * self.k + j, 0 : num_classes] = min_sample
+                    triplets[i * num_sam + k * self.k + j, num_classes : num_classes + num_classes] = cls_score[hard_neg[1]] if hard_neg[1] > 0 else hard_neg[0]
+                    triplets[i * num_sam + k * self.k + j, 2 * num_classes : 3 * num_classes] = cls_score[hard_pos[1]] if hard_pos[1] > 0 else hard_pos[0]
 
         print(triplets) 
             
