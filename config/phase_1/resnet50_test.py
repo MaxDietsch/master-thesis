bb9d997e0d86b774333502513839e1269fa5ec43
Max Dietsch <diedietschies@gmail.com>
2024-03-04 09:24:23 +0100

    crl test

--- config/phase_1/resnet50_test.py
+++ config/phase_1/resnet50_test.py
@@ -1,9 +1,14 @@
+
+
 _base_ = [
         '../../model/resnet50.py',
         '../../data/phase1/test.py',
         #'../../schedule/sgd0_01.py',
         '../../runtime/default.py'
 ]
+
+val_cfg = dict()
+test_cfg = dict()
 
 load_from = None
 resume = False
