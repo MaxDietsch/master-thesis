ed2988216158aa3058e281690566a933f799f77f
Max <diedietschies@gmail.com>
2024-03-12 08:55:44 +0100

    finalize test

--- tools_created/print_test_results.py
+++ tools_created/print_test_results.py
@@ -38,7 +38,7 @@ def find_json_values(root_dir):
                                             recall = data.get('single-label/recall_classwise', 'N/A')
                                             precision = data.get('single-label/precision_classwise', 'N/A')
                                             f1_score = data.get('single-label/f1-score_classwise', 'N/A')
-                                            acc[d1].append(accuracy)
+                                            acc[d1].append([accuracy])
                                             rec[d1].append(recall)
                                             prec[d1].append(precision)
                                             f1[d1].append(f1)
