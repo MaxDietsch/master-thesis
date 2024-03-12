5894623344b63c43e9c63727dc88da0820685f32
Max <diedietschies@gmail.com>
2024-03-12 08:06:38 +0100

    finalize test

--- mmpretrain/models/losses/dos_loss.py
+++ mmpretrain/models/losses/dos_loss.py
@@ -24,7 +24,37 @@ class DOSLoss(nn.Module):
             n (torch.Tensor): The deep features of the nearest neighbours of the input
             w (torch.Tensor): The weights for the given input
         """
+    
+        #"""Pytorchify:
+        f_loss = 0
+        g_loss = 0
+        batch_size = deep_feats[0].shape[0]
+        for i in range(batch_size):
+            rho.append((torch.empty(1)).to(torch.device("cuda")))
+            if w[i].numel() == 0: 
+                # this sample has no overloaded instance
+                continue
+            else:
+                #print(w[i].shape)
+                #print(n[i].shape)
+                #print(deep_feats[0][i].shape)
+                #print((deep_feats[0][i] - n[i]).shape)
+                rho[i] = -w[i] @ torch.linalg.norm(deep_feats[0][i] - n[i], dim = 1, keepdim = True)
+                rho[i] = torch.exp(rho[i])
+                rho[i] = rho[i] / torch.sum(rho[i])
+        #print(rho)
 
+        for i in range(batch_size):
+            f_loss += -w[i] @ torch.linalg.norm(deep_feats[0][i] - n[i], dim = 1, keepdim = True)
+            g_loss += rho[i] @ self.ce_loss(cls_score, target) 
+        loss = f_loss + g_loss
+        return loss
+
+
+
+
+
+        """
         if len(n) == 0:
             loss = self.ce_loss(cls_score[0], target)
 
@@ -33,26 +63,7 @@ class DOSLoss(nn.Module):
             g_loss = 0
             # helper for g_loss
             rho = []
-            
-            #"""Pytorchify:
-            batch_size = deep_feats[0].shape[0]
-            for i in range(batch_size):
-                rho.append((torch.empty(1)).to(torch.device("cuda")))
-                if w[i].numel() == 0: 
-                    # this sample has no overloaded instance
-                    continue
-                else:
-                    #print(w[i].shape)
-                    #print(n[i].shape)
-                    #print(deep_feats[0][i].shape)
-                    #print((deep_feats[0][i] - n[i]).shape)
-                    rho[i] = -w[i] @ torch.linalg.norm(deep_feats[0][i] - n[i], dim = 1, keepdim = True)
-                    rho[i] = torch.exp(rho[i])
-                    rho[i] = rho[i] / torch.sum(rho[i])
-                    
-            print(rho)
-
-
+                        
             for idy, w_i in enumerate(w):
                 rho.append((torch.zeros(len(n))).to(torch.device("cuda")))
                 for idx, v_i in enumerate(n):
@@ -66,8 +77,9 @@ class DOSLoss(nn.Module):
                 for idx, v_i in enumerate(n):
                     f_loss += w_i[idx] * torch.linalg.norm(deep_feats[0] - v_i)
                     g_loss += rho[idy][idx] * self.ce_loss(cls_score[idx].unsqueeze(0), target)
-
+            
             loss = g_loss + f_loss
+            """
 
         return loss
                 
