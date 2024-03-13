a89fc263496e8efdd083a3cd91c34185b8f0eac8
diedietschies <diedietschies@gmail.com>
2024-03-13 08:47:46 +0100

    conf_matrix

--- tools_created/get_conf_matrix.py
+++ tools_created/get_conf_matrix.py
@@ -33,9 +33,7 @@ def find_json_values(root_dir):
 def calculate_average():
 
     for key in cms:
-        print(torch.stack(cms[key]))
         cm_mean = torch.mean(torch.stack(cms[key]), dim = 0)
-        print(cm_mean)
         cm_std = torch.std(torch.stack(cms[key]), dim = 0)
         
         
