c41f90d0137cddf96937ecbb57e644eaa7c84c2f
diedietschies <diedietschies@gmail.com>
2024-03-14 07:49:07 +0100

    aug test

---
+++ config/phase2/efficientnet_b4_aug2.py
@@ -0,0 +1,9 @@
+_base_ = [
+        '../../model/efficientnet_b4.py',
+        '../../data/phase2/bepn16_aug2.py',
+        '../../schedule/sgd0_01.py',
+        '../../runtime/default.py'
+        ]
+
+load_from = None
+resume = False
