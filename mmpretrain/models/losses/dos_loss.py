1f531cc2a697fda65be1e13314181165bfed9434
diedietschies <diedietschies@gmail.com>
2024-03-15 09:29:29 +0100

    aug exp

--- mmpretrain/models/losses/dos_loss.py
+++ mmpretrain/models/losses/dos_loss.py
@@ -77,7 +77,9 @@ class DOSLoss(nn.Module):
                 # for not oversampled instances take the normal loss
                 n_loss += self.ce_loss(cls_score[i], target[i].unsqueeze(dim=0))
 
-        print(f_loss, g_loss, n_loss)
+        print(f'n_loss: {n_loss}')
+        print(f'f_loss: {f_loss}')
+        print(f'g_loss: {g_loss}')
         loss = f_loss + g_loss+ n_loss
 
 
