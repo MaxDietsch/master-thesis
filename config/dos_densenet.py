dcc0480b999ca06a6a6a4be3644d2e41cea976f5
diedietschies <diedietschies@gmail.com>
2024-03-15 10:29:43 +0100

    dos

--- config/dos_densenet.py
+++ config/dos_densenet.py
@@ -7,6 +7,5 @@ _base_ = [
         '../runtime/default.py'
 ]
 
-#load_from = '../work_dirs/densetnet121_sgd_bepnwom_default/epoch_400.pth'
-load_from = None
+load_from = '../work_dirs/densetnet121_sgd_bepnwom_default/epoch_400.pth'
 resume = False
