05f29fffec2f4d1c1a4f56ea951ba8a2c39119c1
Max Dietsch <diedietschies@gmail.com>
2024-03-18 10:05:08 +0100

    dos

--- mmpretrain/models/heads/dos_head.py
+++ mmpretrain/models/heads/dos_head.py
@@ -59,7 +59,7 @@ class DOSHead(LinearClsHead):
         else:
             print(self((n, )).shape)
             print(self((n.unsqueeze(1), )).shape)
-            cls_score.append(yself((n, )))
+            cls_score.append(self((n, )))
         """
 
         cls_score = []
