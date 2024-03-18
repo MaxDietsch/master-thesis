022e88139ad4b005116e15ffa1a2583326e0477f
Max Dietsch <diedietschies@gmail.com>
2024-03-18 09:32:36 +0100

    dos

--- mmpretrain/models/losses/dos_loss.py
+++ mmpretrain/models/losses/dos_loss.py
@@ -72,7 +72,8 @@ class DOSLoss(nn.Module):
             # rho is of shape r x k -> result will be r x 1 (for each weight vector) -> sum over it 
             # implements rho(vi, wi) * H(g(vi), y) (-> sum for every i), where g(vi) is prediction for oversamples feature and y is ground truth
             for score in cls_score:
-                print(torch.tensor([self.ce_loss(score, target) for score in cls_score]))
+                print(score)
+                print(torch.tensor([self.ce_loss(score, target)))
             loss += torch.sum(rho @ torch.tensor([self.ce_loss(score, target) for score in cls_score]).to(torch.device("cuda")))
             
             #print(cls_score.shape)
