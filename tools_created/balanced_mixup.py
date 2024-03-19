c73fb9e0af28d8666c78a60bb5eed93218d7808d
Max Dietsch <diedietschies@gmail.com>
2024-03-19 16:41:43 +0100

    args

--- tools_created/balanced_mixup.py
+++ tools_created/balanced_mixup.py
@@ -47,11 +47,10 @@ for i in range(num_new_samples):
     img_si = cv2.imread(img_si)
     img_si = cv2.cvtColor(img_si, cv2.COLOR_BGR2RGB)
     img_si = transforms.ToTensor()(img_si)
-    print(img_si.shape)
     img_si = img_si.unsqueeze(0)
     img_si = img_si.unsqueeze(0)
     img_si = nn.functional.interpolate(img_si, size = img_sc.shape, mode = 'trilinear')
-    print(img_si.shape)
+    img_si = img_si.squeeze()
 
     # sample lambda
     l = beta_dist.sample()
