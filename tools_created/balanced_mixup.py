dab52eef458640ef3d0b8b6e22372b7e782b984b
Max Dietsch <diedietschies@gmail.com>
2024-03-19 16:28:18 +0100

    args

--- tools_created/balanced_mixup.py
+++ tools_created/balanced_mixup.py
@@ -1,6 +1,7 @@
 import torch
 import cv2
 from torchvision import transforms
+import torch.nn as nn
 """
 This file takes in a dataset (so the information about it). It uses the balancedMixUp strategy proposed in: 
 Balanced-MixUp for Highly Imbalanced Medical Image Classification by Galdran et al. 
@@ -38,14 +39,14 @@ for i in range(num_new_samples):
     img_sc = paths[sc][torch.randint(low = 0, high = len(paths[sc]), size = (1, ))]
     img_sc = cv2.imread(img_sc)
     img_sc = cv2.cvtColor(img_sc, cv2.COLOR_BGR2RGB)
-    img_sc = torch.ToTensor(img_sc)
+    img_sc = transforms.ToTensor(img_sc)
 
     # instance based sampling and reshape image to match dimension of the first image 
     si = cat_dist.sample(sample_shape=(1,))
     img_si = paths[si][torch.randint(low = 0, high = len(paths[si]), size = (1, ))]
     img_si = cv2.imread(img_si)
     img_si = cv2.cvtColor(img_si, cv2.COLOR_BGR2RGB)
-    img_si = torch.ToTensor(img_si)
+    img_si = transforms.ToTensor(img_si)
     img_si = nn.functional.interpolate(img_si, size = img_sc.shape, mode = 'bicubic')
 
     # sample lambda
