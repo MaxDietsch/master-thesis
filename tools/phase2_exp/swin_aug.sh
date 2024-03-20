c8cd7f718e6bdea71386c948c5a41bee970960b9
Max Dietsch <diedietschies@gmail.com>
2024-03-20 07:28:20 +0100

    swin aug

--- tools/phase2_exp/swin_aug.sh
+++ tools/phase2_exp/swin_aug.sh
@@ -2,11 +2,11 @@
 
 cd ..
 
-python train.py ../config/phase2/swin_aug.py --work-dir ../work_dirs/phase2/swin/aug/lr_decr/
+python train.py ../config/phase2/swin_aug.py --lr 0.001 --work-dir ../work_dirs/phase2/swin/aug/0.001/
 
-python train.py ../config/phase2/swin_aug2.py --work-dir ../work_dirs/phase2/swin/aug2/lr_decr/
+python train.py ../config/phase2/swin_aug2.py --lr 0.001 --work-dir ../work_dirs/phase2/swin/aug2/0.001/
 
-python train.py ../config/phase2/swin_aug3.py --work-dir ../work_dirs/phase2/swin/aug3/lr_decr/
+python train.py ../config/phase2/swin_aug3.py --lr 0.001 --work-dir ../work_dirs/phase2/swin/aug3/0.001/
 
-python train.py ../config/phase2/swin_aug4.py --work-dir ../work_dirs/phase2/swin/aug4/lr_decr/
+python train.py ../config/phase2/swin_aug4.py --lr 0.001 --work-dir ../work_dirs/phase2/swin/aug4/0.001/
 
