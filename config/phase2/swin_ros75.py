89db8aff8b60c1e178e4660a5f4dcb40f9390438
diedietschies <diedietschies@gmail.com>
2024-03-22 07:35:48 +0100

    swin ros

---
+++ config/phase2/swin_ros75.py
@@ -0,0 +1,9 @@
+_base_ = [
+        '../../model/swin.py',
+        '../../data/phase2/bepn14_ros75.py',
+        '../../schedule/sgd_decr.py',
+        '../../runtime/default.py'
+        ]
+
+load_from = None
+resume = False
