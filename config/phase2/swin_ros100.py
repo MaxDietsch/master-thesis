2a7bb0c9ab867532051ad97f6c2ba743e6446579
Max Dietsch <diedietschies@gmail.com>
2024-03-27 14:48:30 +0100

    resume

--- config/phase2/swin_ros100.py
+++ config/phase2/swin_ros100.py
@@ -5,5 +5,5 @@ _base_ = [
         '../../runtime/default.py'
         ]
 
-load_from = '../../work_dirs/phase2/swin/ros100/epoch_77.pth'
+load_from = '../../work_dirs/phase2/swin/ros100/lr_0.001/epoch_77.pth'
 resume = True
