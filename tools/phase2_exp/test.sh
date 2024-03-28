3f6fa727574846abc346b4afe75bdd6c41377edb
diedietschies <diedietschies@gmail.com>
2024-03-28 07:34:32 +0100

    swin_ros_test

--- tools/phase2_exp/test.sh
+++ tools/phase2_exp/test.sh
@@ -2,8 +2,8 @@
 
 cd ..
 
-method="aug2"
-model="efficientnet_b4"
+method="ros25"
+model="swin"
 epoch="91 92 93 94 95 96 97 98 99 100"
 lr_array=("lr_0.01" "lr_0.001" "lr_decr")
 #epoch_lr0_01="60 98"
