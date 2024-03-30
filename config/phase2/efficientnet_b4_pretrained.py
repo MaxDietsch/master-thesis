4a1bd4e439f533c6366978142773f22e5546b9f9
diedietschies <diedietschies@gmail.com>
2024-03-30 08:23:39 +0100

    pretrained

---
+++ config/phase2/efficientnet_b4_pretrained.py
@@ -0,0 +1,9 @@
+_base_ = [
+        '../../model/efficientnet_b4_pretrained.py',
+        '../../data/phase1/bepn8.py',
+        '../../schedule/sgd_decr.py',
+        '../../runtime/default.py'
+        ]
+
+load_from = None
+resume = False
