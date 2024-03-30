d8f0d7ce4ad6c71730a715d86716b0deaa396894
diedietschies <diedietschies@gmail.com>
2024-03-30 08:29:59 +0100

    pretrained

--- model/efficientnet_b4_pretrained.py
+++ model/efficientnet_b4_pretrained.py
@@ -1,6 +1,6 @@
 model = dict(
     type='ImageClassifier',
-    backbone=dict(type='EfficientNet', arch='b4', init_cfg = dict(type = 'Pretrained', '../work_dirs/phase2/efficientnet_b4/pretrain/epoch_100.pth', prefix = 'backbone')),
+    backbone=dict(type='EfficientNet', arch='b4', init_cfg = dict(type = 'Pretrained', checkpoint='../work_dirs/phase2/efficientnet_b4/pretrain/epoch_100.pth', prefix = 'backbone')),
     neck=dict(type='GlobalAveragePooling'),
     head=dict(
         type='LinearClsHead',
