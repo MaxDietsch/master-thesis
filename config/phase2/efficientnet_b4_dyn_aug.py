94258f82cd13264db46a35fad1b45276240454fe
diedietschies <diedietschies@gmail.com>
2024-04-11 12:39:43 +0200

    dynamic sampling

---
+++ config/phase2/efficientnet_b4_dyn_aug.py
@@ -0,0 +1,9 @@
+_base_ = [
+        '../../model/efficientnet_b4.py',
+        '../../data/phase2/bepn8_dyn_aug.py',
+        '../../schedule/sgd_decr.py',
+        '../../runtime/default.py'
+        ]
+
+load_from = None
+resume = False
