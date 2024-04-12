63b031c06ea267edcb054f5d98f0d165d714b979
diedietschies <diedietschies@gmail.com>
2024-04-12 18:28:31 +0200

    col

--- tools/phase2_exp/efficientnet_b4_col.sh
+++ tools/phase2_exp/efficientnet_b4_col.sh
@@ -2,15 +2,22 @@
 
 cd ..
 
-python train.py ../config/phase2/efficientnet_b4_col.py --work-dir ../work_dirs/phase2/efficientnet_b4/col/lr_decr/
+#python train.py ../config/phase2/efficientnet_b4_col.py --work-dir ../work_dirs/phase2/efficientnet_b4/col/lr_decr/
 
-python train.py ../config/phase2/efficientnet_b4_col.py --lr 0.01 --work-dir ../work_dirs/phase2/efficientnet_b4/col/lr_0.01/
+#python train.py ../config/phase2/efficientnet_b4_col.py --lr 0.01 --work-dir ../work_dirs/phase2/efficientnet_b4/col/lr_0.01/
 
-python train.py ../config/phase2/efficientnet_b4_col.py --lr 0.001 --work-dir ../work_dirs/phase2/efficientnet_b4/col/lr_0.001/
+#python train.py ../config/phase2/efficientnet_b4_col.py --lr 0.001 --work-dir ../work_dirs/phase2/efficientnet_b4/col/lr_0.001/
 
-python train.py ../config/phase2/efficientnet_b4_col_100.py --work-dir ../work_dirs/phase2/efficientnet_b4/col_100/lr_decr/
+#python train.py ../config/phase2/efficientnet_b4_col_100.py --work-dir ../work_dirs/phase2/efficientnet_b4/col_100/lr_decr/
 
-python train.py ../config/phase2/efficientnet_b4_col_100.py --lr 0.01 --work-dir ../work_dirs/phase2/efficientnet_b4/col_100/lr_0.01/
+#python train.py ../config/phase2/efficientnet_b4_col_100.py --lr 0.01 --work-dir ../work_dirs/phase2/efficientnet_b4/col_100/lr_0.01/
 
-python train.py ../config/phase2/efficientnet_b4_col_100.py --lr 0.001 --work-dir ../work_dirs/phase2/efficientnet_b4/col_100/lr_0.001/
+#python train.py ../config/phase2/efficientnet_b4_col_100.py --lr 0.001 --work-dir ../work_dirs/phase2/efficientnet_b4/col_100/lr_0.001/
+
+
+python train.py ../config/phase2/efficientnet_b4_col_45.py --work-dir ../work_dirs/phase2/efficientnet_b4/col_45/lr_decr/
+
+python train.py ../config/phase2/efficientnet_b4_col_45.py --lr 0.01 --work-dir ../work_dirs/phase2/efficientnet_b4/col_45/lr_0.01/
+
+python train.py ../config/phase2/efficientnet_b4_col_45.py --lr 0.001 --work-dir ../work_dirs/phase2/efficientnet_b4/col_45/lr_0.001/
 
