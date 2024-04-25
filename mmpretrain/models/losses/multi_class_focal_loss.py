971805ff5e1bf5b01d630a0f43a1b5130d4ec755
Max <diedietschies@gmail.com>
2024-04-25 10:51:26 +0200

    focal test

--- mmpretrain/models/losses/multi_class_focal_loss.py
+++ mmpretrain/models/losses/multi_class_focal_loss.py
@@ -8,27 +8,18 @@ from .utils import convert_to_one_hot, weight_reduce_loss
 
 def softmax_focal_loss(pred,
                        target,
-                       weight=None,
                        gamma=2.0,
                        alpha=0.25,
-                       reduction='mean',
-                       avg_factor=None):
+                       ):
     r"""ce focal loss.
 
     Args:
         pred (torch.Tensor): The prediction with shape (N, \*).
         target (torch.Tensor): The ground truth label of the prediction with
             shape (N, \*).
-        weight (torch.Tensor, optional): Sample-wise loss weight with shape
-            (N, ). Defaults to None.
         gamma (float): The gamma for calculating the modulating factor.
             Defaults to 2.0.
         alpha (float): A balanced form for Focal Loss. Defaults to 0.25.
-        reduction (str): The method used to reduce the loss.
-            Options are "none", "mean" and "sum". If reduction is 'none' ,
-            loss is same shape as pred and label. Defaults to 'mean'.
-        avg_factor (int, optional): Average factor that is used to average
-            the loss. Defaults to None.
 
     Returns:
         torch.Tensor: Loss.
@@ -43,14 +34,8 @@ def softmax_focal_loss(pred,
     focal_weight = alpha * (1 - pt).pow(gamma)
     print(focal_weight)
     print(F.cross_entropy(pred, target.long()))
-    loss = F.cross_entropy(
-        pred, target, reduction='none') * focal_weight
-    if weight is not None:
-        assert weight.dim() == 1
-        weight = weight.float()
-        if pred.dim() > 1:
-            weight = weight.reshape(-1, 1)
-    loss = weight_reduce_loss(loss, weight, reduction, avg_factor)
+    loss = pred_probs * focal_weight
+    print(loss)
     exit()
     return loss
 
@@ -64,22 +49,16 @@ class MultiClassFocalLoss(nn.Module):
             Defaults to 2.0.
         alpha (float): The parameter in balanced form of focal
             loss. Defaults to 0.25.
-        reduction (str): The method used to reduce the loss into
-            a scalar. Options are "none" and "mean". Defaults to 'mean'.
         loss_weight (float): Weight of loss. Defaults to 1.0.
     """
 
     def __init__(self,
                  gamma=2.0,
-                 alpha=0.25,
-                 reduction='mean',
-                 loss_weight=1.0):
+                 alpha=0.25):
 
         super(MultiClassFocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        self.reduction = reduction
-        self.loss_weight = loss_weight
 
     def forward(self,
                 pred,
@@ -110,9 +89,6 @@ class MultiClassFocalLoss(nn.Module):
         loss_cls = self.loss_weight * softmax_focal_loss(
             pred,
             target,
-            weight,
             gamma=self.gamma,
-            alpha=self.alpha,
-            reduction=reduction,
-            avg_factor=avg_factor)
+            alpha=self.alpha)
         return loss_cls
