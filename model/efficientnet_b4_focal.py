94ffc2456089f40af3a2498f4b021c814c1840f3
Max <diedietschies@gmail.com>
2024-04-25 10:21:32 +0200

    focal test

---
+++ model/efficientnet_b4_focal.py
@@ -0,0 +1,11 @@
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(type='EfficientNet', arch='b4'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=4,
+        in_channels=1792,
+        loss=dict(type='MultiClassFocalLoss'),
+        topk=(1),
+    ))
