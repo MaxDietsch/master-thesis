754e10c6f31e5d3d662eac997926dd68427e5a18
diedietschies <diedietschies@gmail.com>
2024-04-29 19:07:03 +0200

    eff focal

--- config/phase3/efficientnet_b4_focal_test.py
+++
@@ -1,9 +0,0 @@
-_base_ = [
-        '../../model/efficientnet_b4_focal.py',
-        '../../data/phase2/bepn8.py',
-        '../../schedule/sgd_decr.py',
-        '../../runtime/default.py'
-        ]
-
-load_from = None
-resume = False
--- tools/phase3_exp/efficientnet_b4_focal_default.sh
+++ tools/phase3_exp/efficientnet_b4_focal_default.sh
@@ -2,10 +2,10 @@
 
 cd ..
 
-python train.py ../config/phase3/efficientnet_b4_focal_default.py --work-dir ../work_dirs/phase3/efficientnet_b4/focal_default/lr_decr/
+python train.py ../config/phase3/efficientnet_b4_focal_default.py --work-dir ../work_dirs/phase3/efficientnet_b4/focal_default_aug/lr_decr/
 
-python train.py ../config/phase3/efficientnet_b4_focal_default.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/focal_default/lr_0.01/
+python train.py ../config/phase3/efficientnet_b4_focal_default.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/focal_default_aug/lr_0.01/
 
-python train.py ../config/phase3/efficientnet_b4_focal_default.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/focal_default/lr_0.001/
+python train.py ../config/phase3/efficientnet_b4_focal_default.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/focal_default_aug/lr_0.001/
 
 
