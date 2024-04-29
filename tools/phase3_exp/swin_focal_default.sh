6af983fb38db8d58e61818b223291c52008f28d1
Max <diedietschies@gmail.com>
2024-04-29 08:17:13 +0200

    focal

--- tools/phase3_exp/swin_focal_default.sh
+++ tools/phase3_exp/swin_focal_default.sh
@@ -2,10 +2,10 @@
 
 cd ..
 
-python train.py ../config/phase3/swin_focal_default.py --work-dir ../work_dirs/phase3/swin/focal_default/lr_decr/
+python train.py ../config/phase3/swin_focal_default.py --work-dir ../work_dirs/phase3/swin/focal_default2/lr_decr/
 
-python train.py ../config/phase3/swin_focal_default.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/focal_default/lr_0.01/
+python train.py ../config/phase3/swin_focal_default.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/focal_default2/lr_0.01/
 
-python train.py ../config/phase3/swin_focal_default.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/focal_default/lr_0.001/
+python train.py ../config/phase3/swin_focal_default.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/focal_default2/lr_0.001/
 
 
