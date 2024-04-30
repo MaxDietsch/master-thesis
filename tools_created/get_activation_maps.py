b61efbd6309c8ec8b2e951e39f5f80589ce63365
Max <diedietschies@gmail.com>
2024-04-30 10:03:31 +0200

    cam

--- tools_created/get_activation_maps.py
+++ tools_created/get_activation_maps.py
@@ -14,7 +14,7 @@ DEVICE = torch.device('cuda')
 METHOD = 'EigenCAM'
 AUG_SMOOTH = False
 EIGEN_SMOOTH = False 
-PATH = '../../utils/esophagitis'
+PATH = '../../utils/polyp'
 VIT_LIKE = True
 TARGET_LAYERS = []
 
