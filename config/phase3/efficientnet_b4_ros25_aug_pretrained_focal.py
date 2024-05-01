e7daa5379e4dbce8d2e09575052f9aac88b3df0c
diedietschies <diedietschies@gmail.com>
2024-05-01 13:40:53 +0200

    focal exp

---
+++ config/phase3/efficientnet_b4_ros25_aug_pretrained_focal.py
@@ -0,0 +1,9 @@
+_base_ = [
+        '../../model/efficientnet_b4_pretrained_focal.py',
+        '../../data/phase2/bepn8_ros25.py',
+        '../../schedule/sgd_decr.py',
+        '../../runtime/default.py'
+        ]
+
+load_from = None
+resume = False
---
+++ model/efficientnet_b4_pretrained_focal.py
@@ -0,0 +1,11 @@
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(type='EfficientNet', arch='b4', init_cfg = dict(type = 'Pretrained', checkpoint='../work_dirs/phase2/efficientnet_b4/pretrain/epoch_100.pth', prefix = 'backbone')),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=4,
+        in_channels=1792,
+        loss=dict(type='MultiClassFocalLoss'),
+        topk=(1),
+    ))
