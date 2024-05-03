3e0f10b5e403640845c8294ff891fc051725b6f7
diedietschies <diedietschies@gmail.com>
2024-05-03 15:05:08 +0200

    batch fl

--- mmpretrain/models/losses/multi_class_focal_loss.py
+++ mmpretrain/models/losses/multi_class_focal_loss.py
@@ -33,7 +33,7 @@ def softmax_focal_loss(pred,
     #print(pt)
     focal_weight = alpha * (1 - pt).pow(gamma)
     #print(focal_weight)
-    print(-focal_weight * torch.log(pt))
+    #print(-focal_weight * torch.log(pt))
     loss = (-focal_weight * torch.log(pt)).mean()
     #print(loss)
     return loss
