79e928fc08cea0c5954d7768c6a5c32be22d3ec1
diedietschies <diedietschies@gmail.com>
2024-05-07 07:38:45 +0200

    batch fl

--- tools/phase3_exp/efficientnet_b4_ros_aug_pretrained_focal2.sh
+++ tools/phase3_exp/efficientnet_b4_ros_aug_pretrained_focal2.sh
@@ -11,21 +11,21 @@ python train.py ../config/phase3/efficientnet_b4_ros75_aug_pretrained_focal2.py 
 python train.py ../config/phase3/efficientnet_b4_ros100_aug_pretrained_focal2.py --work-dir ../work_dirs/phase3/efficientnet_b4/ros100_aug_pretrained_focal2/lr_decr/
 
 
-#python train.py ../config/phase3/efficientnet_b4_ros25_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros25_aug_pretrained_focal2/lr_0.01/
+python train.py ../config/phase3/efficientnet_b4_ros25_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros25_aug_pretrained_focal2/lr_0.01/
 
-#python train.py ../config/phase3/efficientnet_b4_ros50_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros50_aug_pretrained_focal2/lr_0.01/
+python train.py ../config/phase3/efficientnet_b4_ros50_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros50_aug_pretrained_focal2/lr_0.01/
 
-#python train.py ../config/phase3/efficientnet_b4_ros75_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros75_aug_pretrained_focal2/lr_0.01/
+python train.py ../config/phase3/efficientnet_b4_ros75_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros75_aug_pretrained_focal2/lr_0.01/
 
-#python train.py ../config/phase3/efficientnet_b4_ros100_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros100_aug_pretrained_focal2/lr_0.01/
+python train.py ../config/phase3/efficientnet_b4_ros100_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/efficientnet_b4/ros100_aug_pretrained_focal2/lr_0.01/
 
 
-#python train.py ../config/phase3/efficientnet_b4_ros25_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros25_aug_pretrained_focal2/lr_0.001/
+python train.py ../config/phase3/efficientnet_b4_ros25_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros25_aug_pretrained_focal2/lr_0.001/
 
-#python train.py ../config/phase3/efficientnet_b4_ros50_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros50_aug_pretrained_focal2/lr_0.001/
+python train.py ../config/phase3/efficientnet_b4_ros50_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros50_aug_pretrained_focal2/lr_0.001/
 
-#python train.py ../config/phase3/efficientnet_b4_ros75_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros75_aug_pretrained_focal2/lr_0.001/
+python train.py ../config/phase3/efficientnet_b4_ros75_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros75_aug_pretrained_focal2/lr_0.001/
 
-#python train.py ../config/phase3/efficientnet_b4_ros100_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros100_aug_pretrained_focal2/lr_0.001/
+python train.py ../config/phase3/efficientnet_b4_ros100_aug_pretrained_focal2.py --lr 0.001 --work-dir ../work_dirs/phase3/efficientnet_b4/ros100_aug_pretrained_focal2/lr_0.001/
 
 
