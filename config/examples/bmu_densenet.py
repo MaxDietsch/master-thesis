ba6bc688ac4f1637481e9e47f147bcd0508a80d6
Max <diedietschies@gmail.com>
2024-05-08 08:18:51 +0200

    bmu

--- config/examples/bmu_densenet.py
+++ config/examples/bmu_densenet.py
@@ -1,5 +1,5 @@
 _base_ = [
-        '../model/densenet121.py',
+        '../model/bmu_densenet121.py',
         '../data/bepnwom2.py',
         '../schedule/bmu_sgd.py',
         '../runtime/default.py'
---
+++ model/bmu_densenet121.py
@@ -0,0 +1,10 @@
+model = dict(
+    backbone=dict(arch='121', type='DenseNet'),
+    head=dict(
+        in_channels=1024,
+        loss=dict(loss_weight=1.0, type='BMULoss', use_soft = False),
+        num_classes=4,
+        topk=1,
+        type='LinearClsHead'),
+    neck=dict(type='GlobalAveragePooling'),
+    type='ImageClassifier')
