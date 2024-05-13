e212821f0edf6dfedc05ac51776ffe10cacaa618
Max Dietsch <diedietschies@gmail.com>
2024-05-13 08:36:39 +0200

    cosen

--- model/efficientnet_b4_cosen.py
+++ model/efficientnet_b4_cosen.py
@@ -6,6 +6,6 @@ model = dict(
         type='CoSenLinearClsHead',
         num_classes=4,
         in_channels=1792,
-        loss=dict(type='CrossEntropyLoss', num_classes = 4, learning_rate = 0.1),
+        loss=dict(type='CoSenCrossEntropyLoss', num_classes = 4, learning_rate = 0.1),
         topk=(1),
     ))
