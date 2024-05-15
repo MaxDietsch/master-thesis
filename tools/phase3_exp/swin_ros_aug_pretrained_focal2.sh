d06b42a149cf2d16afad469aa36331a82d40e781
Max Dietsch <diedietschies@gmail.com>
2024-05-15 12:36:07 +0200

    focal + cosen

--- tools/phase3_exp/swin_ros_aug_pretrained_focal2.sh
+++ tools/phase3_exp/swin_ros_aug_pretrained_focal2.sh
@@ -8,10 +8,10 @@ cd ..
 
 #python train.py ../config/phase3/swin_ros75_aug_pretrained_focal2.py --work-dir ../work_dirs/phase3/swin/ros75_aug_pretrained_focal2/lr_decr/
 
-python train.py ../config/phase3/swin_ros100_aug_pretrained_focal2.py --work-dir ../work_dirs/phase3/swin/ros100_aug_pretrained_focal2/lr_decr/
+#python train.py ../config/phase3/swin_ros100_aug_pretrained_focal2.py --work-dir ../work_dirs/phase3/swin/ros100_aug_pretrained_focal2/lr_decr/
 
 
-python train.py ../config/phase3/swin_ros25_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros25_aug_pretrained_focal2/lr_0.01/
+#python train.py ../config/phase3/swin_ros25_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros25_aug_pretrained_focal2/lr_0.01/
 
 python train.py ../config/phase3/swin_ros50_aug_pretrained_focal2.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros50_aug_pretrained_focal2/lr_0.01/
 
