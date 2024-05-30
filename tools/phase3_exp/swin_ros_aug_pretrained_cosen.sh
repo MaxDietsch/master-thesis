c000d33da8c4dbeb19a7c9f85681bff806271560
diedietschies <diedietschies@gmail.com>
2024-05-30 08:57:38 +0200

    swin cosen

--- tools/phase3_exp/swin_ros_aug_pretrained_cosen.sh
+++ tools/phase3_exp/swin_ros_aug_pretrained_cosen.sh
@@ -11,21 +11,21 @@ cd ..
 #python train.py ../config/phase3/swin_ros100_aug_pretrained_cosen.py --work-dir ../work_dirs/phase3/swin/ros100_aug_pretrained_cosen/lr_decr/
 
 
-python train.py ../config/phase3/swin_ros25_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros25_aug_pretrained_cosen/lr_0.01/
+#python train.py ../config/phase3/swin_ros25_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros25_aug_pretrained_cosen/lr_0.01/
 
-python train.py ../config/phase3/swin_ros50_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros50_aug_pretrained_cosen/lr_0.01/
+#python train.py ../config/phase3/swin_ros50_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros50_aug_pretrained_cosen/lr_0.01/
 
-python train.py ../config/phase3/swin_ros75_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros75_aug_pretrained_cosen/lr_0.01/
+#python train.py ../config/phase3/swin_ros75_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros75_aug_pretrained_cosen/lr_0.01/
 
-python train.py ../config/phase3/swin_ros100_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros100_aug_pretrained_cosen/lr_0.01/
+#python train.py ../config/phase3/swin_ros100_aug_pretrained_cosen.py --lr 0.01 --work-dir ../work_dirs/phase3/swin/ros100_aug_pretrained_cosen/lr_0.01/
 
 
-#python train.py ../config/phase3/swin_ros25_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros25_aug_pretrained_cosen/lr_0.001/
+python train.py ../config/phase3/swin_ros25_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros25_aug_pretrained_cosen/lr_0.001/
 
-#python train.py ../config/phase3/swin_ros50_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros50_aug_pretrained_cosen/lr_0.001/
+python train.py ../config/phase3/swin_ros50_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros50_aug_pretrained_cosen/lr_0.001/
 
-#python train.py ../config/phase3/swin_ros75_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros75_aug_pretrained_cosen/lr_0.001/
+python train.py ../config/phase3/swin_ros75_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros75_aug_pretrained_cosen/lr_0.001/
 
-#python train.py ../config/phase3/swin_ros100_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros100_aug_pretrained_cosen/lr_0.001/
+python train.py ../config/phase3/swin_ros100_aug_pretrained_cosen.py --lr 0.001 --work-dir ../work_dirs/phase3/swin/ros100_aug_pretrained_cosen/lr_0.001/
 
 
