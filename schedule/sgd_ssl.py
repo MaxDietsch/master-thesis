7a1463391fba29d37d0cc17b7b3dd4647a203f16
diedietschies <diedietschies@gmail.com>
2024-06-06 09:01:31 +0200

    ssl

--- schedule/sgd_ssl.py
+++ schedule/sgd_ssl.py
@@ -1,5 +1,5 @@
 optim_wrapper = dict(
-    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
+    optimizer=dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001))
 
 param_scheduler = dict(
     type='MultiStepLR', by_epoch=True, milestones=[30, 60, 90], gamma=0.5)
