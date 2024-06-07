69c808e7da091f49a0ad2a13151964906cdfffe4
diedietschies <diedietschies@gmail.com>
2024-06-07 11:13:37 +0200

    ssl

---
+++ config/phase4/swin_moco.py
@@ -0,0 +1,9 @@
+_base_ = [
+        '../../model/swin_mocov2.py',
+        '../../data/phase4/selfsup14.py',
+        '../../schedule/sgd_ssl.py',
+        '../../runtime/default.py'
+        ]
+
+load_from = None
+resume = False
--- model/swin_mocov2.py
+++ model/swin_mocov2.py
@@ -1,6 +1,6 @@
 model = dict(
     type='MoCo',
-    queue_len=65536,
+    queue_len=512,
     feat_dim=128,
     momentum=0.999,
     backbone=dict(
