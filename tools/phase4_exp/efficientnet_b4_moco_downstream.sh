fdf06d645ea10e491b5c21530d36ddc54386c165
Max Dietsch <diedietschies@gmail.com>
2024-06-10 18:21:57 +0200

    moco downstream

--- tools/phase4_exp/efficientnet_b4_moco_downstream.sh
+++ tools/phase4_exp/efficientnet_b4_moco_downstream.sh
@@ -2,10 +2,10 @@
 
 cd ..
 
-python train.py ../config/phase2/efficientnet_b4_pretrained_moco.py --work-dir ../work_dirs/phase4/efficientnet_b4/pretrained_moco/lr_decr/
+python train.py ../config/phase4/efficientnet_b4_pretrained_moco.py --work-dir ../work_dirs/phase4/efficientnet_b4/pretrained_moco/lr_decr/
 
-python train.py ../config/phase2/efficientnet_b4_pretrained_moco.py --lr 0.01 --work-dir ../work_dirs/phase4/efficientnet_b4/pretrained_moco/lr_0.01/
+python train.py ../config/phase4/efficientnet_b4_pretrained_moco.py --lr 0.01 --work-dir ../work_dirs/phase4/efficientnet_b4/pretrained_moco/lr_0.01/
 
-python train.py ../config/phase2/efficientnet_b4_pretrained_moco.py --lr 0.001 --work-dir ../work_dirs/phase4/efficientnet_b4/pretrained_moco/lr_0.001/
+python train.py ../config/phase4/efficientnet_b4_pretrained_moco.py --lr 0.001 --work-dir ../work_dirs/phase4/efficientnet_b4/pretrained_moco/lr_0.001/
 
 
