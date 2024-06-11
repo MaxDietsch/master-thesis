53eea2b526ea5192296b760e978a215fc4ad9bb6
Max Dietsch <diedietschies@gmail.com>
2024-06-11 09:40:28 +0200

    relative loc test

--- model/efficientnet_b4_relative_loc.py
+++ model/efficientnet_b4_relative_loc.py
@@ -7,7 +7,7 @@ model = dict(
         out_channels=3584,
         with_avg_pool=True),
     head=dict(
-        type='LinearHead',
+        type='LinearClsHead',
         loss=dict(type='CrossEntropyLoss'),
         in_channels=3584,
         num_classes=8,
