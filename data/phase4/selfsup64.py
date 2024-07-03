fcd6f473b10f85128dad7413e3aa8da3292fa248
Max <diedietschies@gmail.com>
2024-07-03 13:09:01 +0200

    eff 128

--- data/phase4/selfsup64.py
+++ data/phase4/selfsup64.py
@@ -39,8 +39,8 @@ train_pipeline = [
 ]
 
 train_dataloader = dict(
-    batch_size=4,
-    num_workers=4,
+    batch_size=64,
+    num_workers=8,
     drop_last=True,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
